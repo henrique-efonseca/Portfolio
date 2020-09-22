33ec3e920775b820622bf197fb0f99a3b814d1b3
Henrique Fonseca <henrique.efonseca@gmail.com>
2020-09-22 16:36:18 +0100

    initial commit

---
+++ Sudoku/working.py
@@ -0,0 +1,90 @@
+board = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+]
+
+
+# Function to backtracking
+def solve(board):
+
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range( 1, 10):
+        if is_valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+            
+            board [row][col] = 0
+
+
+
+# Function to print the board (not necessary)
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 :
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(board[0]) ):
+            if j % 3 == 0 :
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+
+# Function to
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                # row, col
+                return (i, j)  
+
+    return None
+
+
+# Function to
+def is_valid(board, num, pos):
+
+    # Check row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if board[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
+            
+print_board(board)
+solve(board)
+print("\n solving sudoku... \n")
+
+print_board(board)
